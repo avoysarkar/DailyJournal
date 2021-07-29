39b77bc3891787b7af142ee4f65cbce663548a5b
avoysarkar <73129752+avoysarkar@users.noreply.github.com>
2021-07-29 23:53:23 +0530

    Add files via upload
    Added Mongoose

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 var _ = require('lodash');
+var mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -13,15 +14,30 @@ const app = express();
 
 const posts = [];
 
+const postSchema = {
+  postTitle: String,
+  postBody: String
+};
+
+const Post = mongoose.model("Post",postSchema);
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb+srv://admin-avoy:Test1999@cluster0.udqtl.mongodb.net/journalDB",{useNewUrlParser:true});
+// mongoose.connect("mongodb://localhost:27017/journalDB",{useNewUrlParser:true});
+
 app.get("/",function(req,res){
-    res.render("home",{
-      StrtContent: homeStartingContent,
-      objectBody: posts       
+    
+    Post.find({},function(err,posts){
+      if(!err){
+        res.render("home",{
+          StrtContent: homeStartingContent,
+          objectBody: posts       
+        });  
+      }
     });
 });
 
@@ -42,36 +58,45 @@ app.get("/compose",function(req,res){
 });
 
 app.post("/compose",function(req,res){
-  let post = {
+  let postNew = new Post({
     postBody: req.body.postBody,
-    title: req.body.title
-  }
-  posts.push(post);
-  res.redirect("/");
+    postTitle: req.body.title
+  });
+  
+  // posts.push(post);
+  postNew.save(function(err){
+    if(!err){
+      res.redirect("/");
+    }
+  });
 });
 
-app.get("/posts/:postName", function(req,res){
-    posts.forEach(function(post){
-      if(_.lowerCase(post.title) === _.lowerCase(req.params.postName)){
+app.get("/posts/:postId", function(req,res){
+
+    Post.find({_id:req.params.postId},function(err,posts){
+      if(!err){
         res.render("post",{
-           content: post.postBody,
-           heading: post.title
-         });
-         console.log(post.postBody +" "+ post.title);
-      }else{
-        console.log("Match Not Found");
+          content: posts[0].postBody,
+          heading: posts[0].postTitle
+        });
+        console.log(posts[0].postBody +" "+ posts[0].postTitle);
       }
-    })
-});
+    });
 
+    // posts.forEach(function(post){
+    //   if(_.lowerCase(post.title) === _.lowerCase(req.params.postName)){
+    //     res.render("post",{
+    //        content: post.postBody,
+    //        heading: post.title
+    //      });
+    //      console.log(post.postBody +" "+ post.title);
+    //   }else{
+    //     console.log("Match Not Found");
+    //   }
+    // });
 
+  });
 
-
-
-
-
-
-
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
